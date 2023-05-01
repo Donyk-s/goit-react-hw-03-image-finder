fd6ac0037597aa21db7d5753c6a79796d5a0d5d7
Sergey Donyk <doncarlos@i.ua>
2023-05-02 02:06:11 +0300

    axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Searchbar } from './searchbar/Searchbar';
+import axios from 'axios';
 
 export class App extends Component {
   state = {
@@ -11,9 +12,28 @@ export class App extends Component {
     per_page: 12,
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    const myApiKey = '34476830-b52e87f2018fae84058c602d8';
+    if (this.state.search !== prevState.search) {
+      const { search, page, per_page } = this.state;
+      const url = `https://pixabay.com/api/?q=${search}&page=${page}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
+
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response.data.hits);
+          //  const {webformatURL, largeImageURL} = response.data.hits
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+
   handleSubmitSearch = searchData => {
     const { search } = searchData;
     this.setState({ search });
+    console.log(search);
   };
 
   render() {
--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import css from './/Search.module.css';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+const SearchInputSchema = Yup.object().shape({
+  search: Yup.string('Enter more than 1 character')
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('A term value must be entered'),
+});
 
 export const Searchbar = ({ onSubmit }) => {
   function handleSubmit(values, { resetForm }) {
@@ -13,13 +20,17 @@ export const Searchbar = ({ onSubmit }) => {
 
   return (
     <div>
-      <Formik initialValues={{ search: '' }} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={{ search: '' }}
+        validationSchema={SearchInputSchema}
+        onSubmit={handleSubmit}
+      >
         <header className={css.Searchbar}>
           <Form className={css.SearchForm}>
             <button type="submit" className={css.SearchForm__button}>
               <span className={css.button__label}>Search</span>
             </button>
-
+            <ErrorMessage name="search" component="div" />
             <Field
               name="search"
               className={css.SearchForm__input}
