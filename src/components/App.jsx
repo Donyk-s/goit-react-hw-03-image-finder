b478e369fd77aebd032e979d231a134617f24ab1
Sergey Donyk <doncarlos@i.ua>
2023-05-03 02:41:23 +0300

    loadMorreBtn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,83 @@
 import React, { Component } from 'react';
 import { Searchbar } from './searchbar/Searchbar';
-import { ImageGallery } from '..//components/imageGallery/ImageGallery';
+import { ImageGallery } from '../components/imageGallery/ImageGallery';
+import Button from '../components/button/Button';
 import css from './App.module.css';
 import axios from 'axios';
 
 export class App extends Component {
   state = {
-    search: [],
+    search: '',
     id: '',
     webformatURL: '',
     largeImageURL: '',
     page: 1,
     per_page: 12,
     hits: [],
+    loaded: false,
   };
 
   componentDidMount() {
+    this.fetchImages();
+    this.setState({ loaded: true });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.search !== prevState.search) {
+      this.setState({ page: 1, hits: [] }, () => {
+        this.fetchImages();
+      });
+    }
+  }
+
+  fetchImages = () => {
     const myApiKey = '34476830-b52e87f2018fae84058c602d8';
-    const url = `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.page}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.per_page}`;
+    const { search, page, per_page } = this.state;
+    const url = `https://pixabay.com/api/?q=${search}&page=${page}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
 
     axios
       .get(url)
       .then(response => {
-        this.setState({ hits: response.data.hits });
+        this.setState(prevState => ({
+          hits: [...prevState.hits, ...response.data.hits],
+        }));
       })
       .catch(error => {
         console.log(error);
       });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const myApiKey = '34476830-b52e87f2018fae84058c602d8';
-    if (this.state.search !== prevState.search) {
-      const { search, page, per_page } = this.state;
-      const url = `https://pixabay.com/api/?q=${search}&page=${page}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
-
-      axios
-        .get(url)
-        .then(response => {
-          console.log(response.data.hits);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  }
+  };
 
   handleSubmitSearch = searchData => {
     const { search } = searchData;
     this.setState({ search });
-    console.log(search);
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      prevState => ({
+        page: prevState.page + 1,
+      }),
+      () => {
+        this.fetchImages();
+      }
+    );
   };
 
   render() {
+    const { hits, loaded } = this.state;
+    const showLoadMore = hits.length > 0;
+
+    if (!loaded) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div className={css.App}>
         <Searchbar onSubmit={this.handleSubmitSearch} />
-        <ImageGallery hits={this.state.hits} />
+        <ImageGallery hits={hits} />
+        {showLoadMore && (
+          <Button onClick={this.handleLoadMore} text="Load more" />
+        )}
       </div>
     );
   }
---
+++ src/components/button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadMoreButton = ({ onClick }) => (
+  <button type="button" onClick={onClick}>
+    Load more
+  </button>
+);
+
+export default LoadMoreButton;
